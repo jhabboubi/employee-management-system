1e68c34993a30c5d212b9342e40cc768fa019596
Jafer Alhaboubi <habboubi@gmail.com>
2022-03-16 13:02:40 -0500

    init project

---
+++ src/test/java/com/em/system/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.em.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
