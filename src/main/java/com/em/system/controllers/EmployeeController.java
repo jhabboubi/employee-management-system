30d59c0d774c04d6596cd5bf6907b7ba6c85335c
Jafer Alhaboubi <habboubi@gmail.com>
2022-03-16 22:38:44 -0500

    employee rest controller - get all employees added cross-origin annotation

--- src/main/java/com/em/system/controllers/EmployeeController.java
+++ src/main/java/com/em/system/controllers/EmployeeController.java
@@ -5,12 +5,13 @@ import com.em.system.repositories.EmployeeRepository;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 @RequestMapping("/api/v1/")
 @FieldDefaults(level = AccessLevel.PRIVATE)
